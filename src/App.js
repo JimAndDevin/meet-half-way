df5b1c2092911c1e0e58b5f6ff3d83351299a068
dkadillak <devin.kadillak@gmail.com>
2020-09-03 23:13:54 -0700

    initial push! css grid is straight forward but confusing!

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-const gridContainer = {
-  display: "grid", 
-  columnGap: "50px",
-  rowGap: "50px",
-  gridTemplateColumns: "1fr 1fr 1fr",
-  gridTemplateRows: "1fr 1fr 1fr",
-  transition: "all 1s",
-  backgroundColor: "wheat"
-}
+import UserInfo from './Components/UserInfo/UserInfo';
 
-const PlaceHolderHeader ={
-  backgroundColor: "#123456",
+
+
+const PlaceHolderHeader = styled.header`
+  background-color: #123456;
+  color: #ffffff;
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 1.2rem;
+  grid-column: 1/4;
+  grid-row: 1;
+  border-style: solid;
+  border-color: yellow;
+`
+
+const PlaceHolderFooter = {
+  backgroundColor: "#123",
   color: "#ffffff",
-  padding: "1.4rem",
   border: "none",
-  borderRadius: "5px",
   textTransform: "uppercase",
   textAlign: "center",
   fontSize: "1.2rem",
   gridColumn: "1/4",
-  gridRow: "1"
+  gridRow: "6",
+  borderStyle: "solid"
 }
-
-const PlaceHolderFooter ={
-  backgroundColor: "#123456",
-  color: "#ffffff",
-  padding: "1.4rem",
-  border: "none",
-  borderRadius: "5px",
-  textTransform: "uppercase",
-  textAlign: "center",
-  fontSize: "1.2rem",
-  gridColumn: "1/4",
-  gridRow: "1"
+const UserInfoComponent = {
+  borderStyle: "solid",
+  borderColor: "purple" 
 }
 
 
-function App() {
-  return (
-    <div>
-      <header style={PlaceHolderHeader}>Header</header>
-      <h1 style={{
-        gridColumn: "2/3",
-        gridRow: "2/3",
-        placeSelf: "center"
-      }}>Sup</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {}
+
+
+  render(){
+    const gridContainer = {
+      display: "grid", 
+      columnGap: "25px",
+      rowGap: "40px",
+      gridTemplateColumns: "1fr 2fr 1fr",
+      gridTemplateRows: "1fr 1fr 1fr 2fr 1fr 1fr",
+      transition: "all 1s",
+      backgroundColor: "wheat",
+      width: "100vw",
+      height: "100vh",
+      borderStyle: "solid",
+      borderColor: "red"
+    } 
+
+      return (
+        <div style={gridContainer}>
+          <PlaceHolderHeader>Header</PlaceHolderHeader>
+          <UserInfo user="1" style={{
+            gridColumn: "2/3",
+            gridRow: "4",
+            placeSelf: "center"
+          }}/>
+
+          <footer style={PlaceHolderFooter}>FOOTER</footer>
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Components/UserInfo/UserInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const userInfo = (props) => (
+<input value={"User " + props.user + " location"}></input>    
+);
+
+
+
+export default userInfo;
